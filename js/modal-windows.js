4b6278737dac398b0870df94fc205ae06025aec0
kitark <kratykd@icloud.com>
2025-10-09 17:31:33 +0200

    update js contact modal

--- js/modal-windows.js
+++ js/modal-windows.js
@@ -5,11 +5,35 @@
 const contactModal = document.querySelector(".backdrop-contact-modal");
 const openBtnContactModal = document.querySelector(".open-contact-modal-btn");
 const closeBtnContactModal = document.querySelector(".modal-close-btn");
+const subtitleModal = document.querySelector(".contact-modal-text");
+
+//  JS-code for section Events Booksy
+
+// const eventList = document.querySelector('.event-list');
+// eventList.addEventListener("click", (event) => {
+//   const openBtn = event.target.closest(".open-contact-modal-btn");
+//   if (!openBtn) {
+//     return;
+//   }
+//   const eventItem = openBtn.closest(".event-item");
+//   if (!eventItem) {
+//     return;
+//   }
+//   const eventTitle = eventItem.querySelector(".event-title").textContent;
+//   subtitleModal.textContent = eventTitle;
+
+//   openBtnContactModal();
+// });
+
+function openBtnContactModal() {
+  contactModal.classList.remove("window-is-invisible");
+  document.body.classList.add("not-scrolling-page");
+  document.addEventListener("keydown", onEscKeyPress);
+}
 
 openBtnContactModal.addEventListener("click", () => {
   contactModal.classList.remove("window-is-invisible");
   document.body.classList.add("not-scrolling-page");
-
   document.addEventListener("keydown", onEscKeyPress);
 });
 
