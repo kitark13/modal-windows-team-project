bc317c85cbeda577e9be807ebeb67b8fbc9bf545
kitark <kratykd@icloud.com>
2025-10-09 15:54:50 +0200

    update contact and book modals

--- js/modal-windows.js
+++ js/modal-windows.js
@@ -1,3 +1,7 @@
+/* ----- Contact Modal ----- */
+
+// open/close Contact Modal
+
 const contactModal = document.querySelector(".backdrop-contact-modal");
 const openBtnContactModal = document.querySelector(".open-contact-modal-btn");
 const closeBtnContactModal = document.querySelector(".modal-close-btn");
@@ -5,17 +9,68 @@ const closeBtnContactModal = document.querySelector(".modal-close-btn");
 openBtnContactModal.addEventListener("click", () => {
   contactModal.classList.remove("window-is-invisible");
   document.body.classList.add("not-scrolling-page");
+
+  document.addEventListener("keydown", onEscKeyPress);
 });
 
-closeBtnContactModal.addEventListener("click", () => {
+closeBtnContactModal.addEventListener("click", closeContactModal);
+
+contactModal.addEventListener("click", (event) => {
+  if (event.target === contactModal) {
+    closeContactModal();
+  }
+});
+
+function closeContactModal() {
   contactModal.classList.add("window-is-invisible");
   document.body.classList.remove("not-scrolling-page");
+
+  document.removeEventListener("keydown", onEscKeyPress);
+}
+
+function onEscKeyPress(event) {
+  if (event.key === "Escape") {
+    closeContactModal();
+  }
+}
+
+/* ----- Contact Modal ----- */
+
+// open/close Book Modal
+
+const bookModal = document.querySelector(".backdrop-book-modal");
+const openBookModalBtn = document.querySelector(".open-book-modal-btn");
+const closeBtnBookModal = document.querySelector(".book-modal-close-btn");
+
+openBookModalBtn.addEventListener("click", () => {
+  bookModal.classList.remove("window-is-invisible");
+  document.body.classList.add("not-scrolling-page");
+
+  document.addEventListener("keydown", onEscKeyPressInBookModal);
 });
 
-// const betaAcc = new Accordion(".accordion-container", {
-//   duration: 300,
-//   showMultiple: true,
-// });
+closeBtnBookModal.addEventListener("click", closeBookModal);
+
+bookModal.addEventListener("click", (event) => {
+  if (event.target === bookModal) {
+    closeBookModal();
+  }
+});
+
+function closeBookModal() {
+  bookModal.classList.add("window-is-invisible");
+  document.body.classList.remove("not-scrolling-page");
+
+  document.removeEventListener("keydown", onEscKeyPress);
+}
+
+function onEscKeyPressInBookModal(event) {
+  if (event.key === "Escape") {
+    closeBookModal();
+  }
+}
+
+// Form quantity book
 
 const quantityInput = document.querySelector("#quantity");
 const minusBtn = document.querySelector(".neg-book-btn");
@@ -31,3 +86,8 @@ minusBtn.addEventListener("click", () => {
     quantityInput.value = currentValue - 1;
   }
 });
+
+// const betaAcc = new Accordion(".accordion-container", {
+//   duration: 300,
+//   showMultiple: true,
+// });
