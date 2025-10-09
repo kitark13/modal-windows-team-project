9b3f005c649b2a5472afd1b8b6ccc559b8cac539
kitark <kratykd@icloud.com>
2025-10-09 12:02:11 +0200

    update book modal

--- js/modal-windows.js
+++ js/modal-windows.js
@@ -11,3 +11,23 @@ closeBtnContactModal.addEventListener("click", () => {
   contactModal.classList.add("window-is-invisible");
   document.body.classList.remove("not-scrolling-page");
 });
+
+// const betaAcc = new Accordion(".accordion-container", {
+//   duration: 300,
+//   showMultiple: true,
+// });
+
+const quantityInput = document.querySelector("#quantity");
+const minusBtn = document.querySelector(".neg-book-btn");
+const plusBtn = document.querySelector(".add-book-btn");
+
+plusBtn.addEventListener("click", () => {
+  quantityInput.value = Number(quantityInput.value) + 1;
+});
+
+minusBtn.addEventListener("click", () => {
+  const currentValue = Number(quantityInput.value);
+  if (currentValue > 1) {
+    quantityInput.value = currentValue - 1;
+  }
+});
